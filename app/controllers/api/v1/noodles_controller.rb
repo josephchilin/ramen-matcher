394c1eab8f99c1642421f7d3fa1e35849238bd2c
josephchilin <joseph@josephchilin.com>
2020-06-15 17:29:36 -0400

    test commit

--- app/controllers/api/v1/noodles_controller.rb
+++ app/controllers/api/v1/noodles_controller.rb
@@ -4,27 +4,5 @@ class Api::V1::NoodlesController < ApplicationController
         noodles = Noodle.all
         render json: noodles, except: [:created_at, :updated_at]
     end
-    
-    # def show
-    #     noodle = Noodle.find(params[:id])
-    #     render json: noodle
-    # end
-
-    # def new
-    
-    # end
-    
-    # def create
-    
-    # end
-    
-    # def edit
-    
-    # end
-    
-    # def update
-    
-    # end
-    
 
 end
