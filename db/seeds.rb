6dc6900c6df0c9f58cee9895601dd542b284dc19
josephchilin <joseph@josephchilin.com>
2020-06-18 11:23:06 -0400

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Destroying noodles"
+# puts "Destroying noodles"
 
-Noodle.destroy_all
+# Noodle.destroy_all
 
-puts "Creating noodles"
+# puts "Creating noodles"
 
-noodle_2 = Noodle.create(name: "ichiran", imageUrl: "https://i.imgur.com/xWTTbBx.jpg")
-noodle_3 = Noodle.create(name: "momosan", imageUrl: "https://i.imgur.com/VCbUDIA.jpg")
-noodle_4 = Noodle.create(name: "nakiryu", imageUrl: "https://i.imgur.com/Ii1vbsK.jpg")
-noodle_5 = Noodle.create(name: "tsuta", imageUrl: "https://i.imgur.com/UwPJR2C.jpg")
+# noodle_2 = Noodle.create(name: "ichiran", imageUrl: "https://i.imgur.com/xWTTbBx.jpg")
+# noodle_3 = Noodle.create(name: "momosan", imageUrl: "https://i.imgur.com/VCbUDIA.jpg")
+# noodle_4 = Noodle.create(name: "nakiryu", imageUrl: "https://i.imgur.com/Ii1vbsK.jpg")
+# noodle_5 = Noodle.create(name: "tsuta", imageUrl: "https://i.imgur.com/UwPJR2C.jpg")
 
 puts "Destroying users"
 
@@ -26,13 +26,13 @@ puts "Creating users"
 user_1 = User.create(name: "Yoda", guesses: 10)
 user_2 = User.create(name: "Luke", guesses: 7)
 
-puts "Destroying scores"
+# puts "Destroying scores"
 
-Score.destroy_all
+# Score.destroy_all
 
-puts "Creating scores"
+# puts "Creating scores"
 
-score_1 = Score.create(user_id: user_1.id, guesses: 3)
+# score_1 = Score.create(user_id: user_1.id, guesses: 3)
 
 # create_table "scores", force: :cascade do |t|
 #     t.integer "user_id"
